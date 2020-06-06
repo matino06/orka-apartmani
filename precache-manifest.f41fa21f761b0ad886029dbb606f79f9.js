1aa33ca86bf24dd12764eb96c84c49f783fdbb1c
matino06 <matino0546@gmil.com>
2020-06-06 13:54:50 +0200

    Updates

---
+++ precache-manifest.f41fa21f761b0ad886029dbb606f79f9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905f7416d225a3734cb19072b000313f",
+    "url": "/matino06/orka.git/index.html"
+  },
+  {
+    "revision": "7c6edfdca10f1a6102e4",
+    "url": "/matino06/orka.git/static/css/2.4b5d561d.chunk.css"
+  },
+  {
+    "revision": "59d0d8d8c38740dacf0b",
+    "url": "/matino06/orka.git/static/css/main.25f53bbd.chunk.css"
+  },
+  {
+    "revision": "7c6edfdca10f1a6102e4",
+    "url": "/matino06/orka.git/static/js/2.772ed519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matino06/orka.git/static/js/2.772ed519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d0d8d8c38740dacf0b",
+    "url": "/matino06/orka.git/static/js/main.8a2c2233.chunk.js"
+  },
+  {
+    "revision": "35ea41879c70e513579c",
+    "url": "/matino06/orka.git/static/js/runtime-main.3631834e.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/matino06/orka.git/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/matino06/orka.git/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/matino06/orka.git/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/matino06/orka.git/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/matino06/orka.git/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/matino06/orka.git/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/matino06/orka.git/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/matino06/orka.git/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/matino06/orka.git/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/matino06/orka.git/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/matino06/orka.git/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/matino06/orka.git/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/matino06/orka.git/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/matino06/orka.git/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/matino06/orka.git/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "233b1d86715b7d347e16022da6987b2c",
+    "url": "/matino06/orka.git/static/media/promajna.233b1d86.jpg"
+  },
+  {
+    "revision": "75a4bbaa6482c7762ed026c1ce77c23c",
+    "url": "/matino06/orka.git/static/media/sea.75a4bbaa.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/matino06/orka.git/precache-manifest.f41fa21f761b0ad886029dbb606f79f9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/matino06/orka.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
